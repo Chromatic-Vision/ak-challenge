7d607757052fc2aaa6ba42b49c827b7b0ad8930e
huu-bo <huubhoogweg@gmail.com>
2023-12-30 20:48:15 +0100

    generate and store rle of offset delta

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
+#include "stb_image.h"
+
 #define JPEG_QUALITY 5  // 1-100, 1 is lowest quality
 
 #define CHUNK_TYPE_VIDEO 0
@@ -38,54 +42,7 @@
 //   if value is b1111, add b11110000 to the chunk value
 //   RLE it
 
-// TODO: use jpeg to compress chunks
 // TODO: maybe use jpeg to compress 1-bit?
-
-static uint8_t rgb2h(uint8_t r, uint8_t g, uint8_t b) {
-	uint8_t max = r > g ? r : g;
-	max = b > max ? b : max;
-
-	uint8_t min = r < b ? r : g;
-	min = b < min ? b : min;
-
-	uint8_t delta = max - min;
-	if (delta == 0.0) {
-		return 0;
-	}
-
-	float hue = 0;
-	if (r == max) {
-		hue = (float)(g - b) / (float)delta;
-	} else if (g == max) {
-		hue = 2.0 + (float)(b - r) / (float)delta;
-	} else {
-		hue = 4.0 + (float)(r - g) / (float)delta;
-	}
-
-	hue *= 42.5;
-	if (hue < 0) {
-		hue += 255.0;
-	}
-
-	return (uint8_t)hue;
-}
-static uint8_t rgb2v(uint8_t r, uint8_t g, uint8_t b) {
-	return (uint8_t)(((uint16_t)r + (uint16_t)g + (uint16_t)b) / 3);
-}
-static uint8_t rgb2s(uint8_t r, uint8_t g, uint8_t b) {
-	uint8_t max = r > g ? r : g;
-	max = b > max ? b : max;
-
-	uint8_t min = r < b ? r : g;
-	min = b < min ? b : min;
-
-	uint8_t delta = max - min;
-	if (delta == 0.0) {
-		return 0;
-	}
-
-	return (uint8_t)(((float)delta / 255.0) / ((float)rgb2v(r, g, b) / 255.0) * 255);
-}
 
 #define CHUNK_SIZE 8
 static uint8_t average(uint8_t* data, unsigned int skip, unsigned int stride) {
@@ -108,12 +65,61 @@ static uint8_t average(uint8_t* data, unsigned int skip, unsigned int stride) {
 	return total / (CHUNK_SIZE * CHUNK_SIZE);
 }
 
+struct {
+	unsigned char* buffer;
+	size_t size;
+	size_t cap;
+} buffer;
+
 unsigned int write_size = 0;
-void jpeg_write_callback(void* context, void* data, int size) {
+void jpeg_write_callback(void* context, void* data, int size) { // TODO: also write to a buffer
 	if (fwrite(data, 1, size, context) != size) {
 		fprintf(stderr, "error writing jpeg to file\n");
 	}
 	write_size += size;
+
+	if (buffer.buffer == NULL) {
+		buffer.cap = 128;
+		buffer.buffer = malloc(buffer.cap);
+		if (buffer.buffer == NULL) {
+			fprintf(stderr, "malloc failed\n");
+			exit(1);
+		}
+	}
+	if (buffer.size + size >= buffer.cap) {
+		buffer.cap = buffer.size + size;
+		buffer.buffer = realloc(buffer.buffer, buffer.cap);
+		if (buffer.buffer == NULL) {
+			fprintf(stderr, "realloc failed\n");
+			exit(1);
+		}
+	}
+
+	memcpy(buffer.buffer + buffer.size, data, size);
+	buffer.size += size;
+}
+
+// 1 = succes, 0 = failure
+int write_rle(FILE* fp, int value, int amount) {
+	if (value < 0 || value > 1) {
+		fprintf(stderr, "incorrect rle value %d\n", value);
+		return 0;
+	}
+	if (amount < 0 || amount > 0xF) {
+		if (amount < 0) {
+			fprintf(stderr, "rle negative (%d)\n", amount);
+		} else {
+			fprintf(stderr, "rle too long (%d)\n", amount);
+		}
+		return 0;
+	}
+
+	uint8_t out = (value << 7) | amount;
+	if (fwrite(&out, 1, 1, fp) != 1) {
+		fprintf(stderr, "writing rle output failed\n");
+		return 0;
+	}
+	return 1;
 }
 
 int main(int argc, char** argv) {
@@ -154,13 +160,21 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	buffer.buffer = NULL;
+	buffer.cap = 0;
+	buffer.size = 0;
+
 	uint8_t* image = malloc(out_width*out_height*3);
+	uint8_t* offset = malloc(out_width*out_height);
 	uint8_t* chunks = malloc((out_width / CHUNK_SIZE)*(out_height / CHUNK_SIZE) * 3);
-	if (image == NULL || chunks == NULL) {
+	if (image == NULL || chunks == NULL || offset == NULL) {
 		fprintf(stderr, "allocating image failed\n");
 
 		if (image != NULL) {
 			free(image);
+		}
+		if (offset != NULL) {
+			free(offset);
 		}
 		if (chunks != NULL) {
 			free(chunks);
@@ -170,6 +184,7 @@ int main(int argc, char** argv) {
 		fclose(outfp);
 		return 1;
 	}
+	memset(offset, 0, out_width*out_height);
 
 	{
 		uint8_t header[] = {'a', 'k', '-', 'c'};
@@ -195,10 +210,18 @@ int main(int argc, char** argv) {
 	}
 
 	unsigned int i = 0;
-	while (/*!feof(fp)*/ i < 10) {
-		if (fread(image, 3, out_width * out_height, fp) != out_width * out_height) {
-			fprintf(stderr, "image too small\n");
-			goto fail;
+	while (/*!feof(fp)*/ i < 10 /*1*/) {
+		{
+			size_t s = fread(image, 3, out_width * out_height, fp);
+			if (s != out_width * out_height) {
+				if (s == 0) {
+					fprintf(stderr, "ZEROOOO!!! (assuming this means end of input file)\n");
+					break;
+				}
+
+				fprintf(stderr, "image too small\n");
+				goto fail;
+			}
 		}
 
 		{ // write that this chunk exists
@@ -223,16 +246,13 @@ int main(int argc, char** argv) {
 				uint8_t b = average(&image[x * CHUNK_SIZE + y * CHUNK_SIZE * out_width + 2], 3, out_width);
 
 
-				uint8_t h = rgb2h(r, g, b);
-				chunks[x + y * (out_width / CHUNK_SIZE)] = h;
-				uint8_t s = rgb2s(r, g, b);
-				chunks[x + y * (out_width / CHUNK_SIZE) + 1] = s;
-				uint8_t v = (uint8_t)(((uint16_t)r + (uint16_t)g + (uint16_t)b) / 3);
-				chunks[x + y * (out_width / CHUNK_SIZE) + 2] = v;
+				chunks[x + y * (out_width / CHUNK_SIZE)] = r;
+				chunks[x + y * (out_width / CHUNK_SIZE) + 1] = g;
+				chunks[x + y * (out_width / CHUNK_SIZE) + 2] = b;
 			}
 		}
 
-		// TODO: write chunk size and data
+		// TODO: write chunk size
 
 		{
 			write_size = 0;
@@ -263,6 +283,73 @@ int main(int argc, char** argv) {
 			}
 		}
 
+		{
+			int width, height, channels;
+
+			uint8_t* d_image = stbi_load_from_memory(buffer.buffer, buffer.size, &width, &height, &channels, 3);
+			if (d_image == NULL) {
+				fprintf(stderr, "loading image failed '%s'\n", stbi_failure_reason());
+				goto fail;
+			}
+			if (channels != 3) {
+				fprintf(stderr, "file has incorrect amount of channels %d\n", channels);
+				goto fail;
+			}
+
+			int last = -1;
+			int amount = 0;
+			for (unsigned int y = 0; y < out_height; y++) {
+				for (unsigned int x = 0; x < out_width; x++) {
+					uint8_t* d_rgb = &d_image[x / CHUNK_SIZE + y / CHUNK_SIZE * width];
+					uint8_t d_val = (uint8_t)(((uint16_t)d_rgb[0] + (uint16_t)d_rgb[1] + (uint16_t)d_rgb[2]) / 3);
+
+					uint8_t* a_rgb = &image[x + y * out_width];
+					uint8_t a_val = (uint8_t)(((uint16_t)a_rgb[0] + (uint16_t)a_rgb[1] + (uint16_t)a_rgb[2]) / 3);
+
+					int value = 0;
+					if (d_val + (offset[x + y * out_width] << 4) < a_val) {
+						// TODO: write to file
+						if (x < 100) {
+							printf("1");
+						}
+
+						if (offset[x + y * out_width] < 0xF) {
+							offset[x + y * out_width]++;
+						}
+						value = 1;
+					} else {
+						if (x < 100) {
+							printf("0");
+						}
+
+						if (offset[x + y * out_width] > 0) {
+							offset[x + y * out_width]--;
+						}
+						value = 0;
+					}
+
+					if (value != last) {
+						if (amount > 0) {
+							if (write_rle(outfp, value, amount) == 0) {
+								goto fail; // it has already printed an error message
+							}
+						}
+						last = value;
+						amount = 0;
+					}
+					if (amount == 127) {
+						if (write_rle(outfp, value, amount) == 0) {
+							goto fail; // it has already printed an error message
+						}
+						amount = 0;
+					}
+				}
+				printf("\n");
+			}
+
+			stbi_image_free(d_image);
+		}
+
 		// {
 		// 	unsigned int s = (out_width / CHUNK_SIZE)*(out_height / CHUNK_SIZE) * 3;
 		// 	if (fwrite(chunks, 1, s, outfp) != s) {
@@ -272,6 +359,11 @@ int main(int argc, char** argv) {
 		// }
 
 		printf("loaded image %u\n", i);
+		// {
+		// 	void* temp = last_image;
+		// 	last_image = image;
+		// 	image = temp;
+		// }
 		i++;
 	}
 
@@ -294,6 +386,7 @@ int main(int argc, char** argv) {
 
     defer:
 	free(image);
+	free(offset);
 	free(chunks);
 	fclose(outfp);
 
